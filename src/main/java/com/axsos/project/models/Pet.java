61eab45562207b96f0bf2c75b6b4404669452101
saeed <saidqtaish@gmail.com>
2024-07-13 11:39:24 +0300

    justified models

--- src/main/java/com/axsos/project/models/Pet.java
+++ src/main/java/com/axsos/project/models/Pet.java
@@ -8,12 +8,14 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -39,17 +41,41 @@ public class Pet {
 	@Size(min = 2, message = "Image URL must be at least 2 characters")
 	@Column(columnDefinition = "TEXT")
 	private String imageUrl;
+	
 	@NotNull
 	@Size(min = 5, message = "Description must be at least 5 characters")
 	@Column(columnDefinition = "TEXT")
 	private String description;
+	
 	@Size(min = 3)
 	private String status;
-	@ManyToMany
+	@ManyToMany // this relationship is for favorites
 	@JoinTable(name = "user_pet", joinColumns = @JoinColumn(name = "pet_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
 	private List<User> users;
+	
+	@ManyToOne(fetch = FetchType.LAZY) // this relationship is for adopting pets
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne(fetch = FetchType.LAZY) // this relationship is for shops and pets
+	@JoinColumn(name = "shop_id")
+	private Shop shop;
 
+	public User getUser() {
+		return user;
+	}
 
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Shop getShop() {
+		return shop;
+	}
+
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
 
 	@Column(updatable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -122,7 +148,7 @@ public class Pet {
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
+
 	public List<User> getUsers() {
 		return users;
 	}
